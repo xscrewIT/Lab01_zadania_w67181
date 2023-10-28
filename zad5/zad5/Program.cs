e05fc7a78b6e6b37fafbbe24d700e481045bdcb9
xscrewIT <128988695+xscrewIT@users.noreply.github.com>
2023-10-28 22:28:12 +0200

    zad5

---
+++ zad5/zad5/Program.cs
@@ -0,0 +1,11 @@
+﻿Zadanie5();
+
+static void Zadanie5()
+{
+    Console.WriteLine("Wyswietlam liczby od 20 do 0, z pominieciem 2, 6, 9, 15, 19");
+    for (int i = 20; i >= 0; i--)
+    {
+        if((i == 2) || (i == 6) || (i == 9) || (i == 15) || (i == 19)) continue;
+        else Console.WriteLine(i);
+    }
+}
