6414cbaf117131c87b8c95005cd2edb17b6df26e
xscrewIT <128988695+xscrewIT@users.noreply.github.com>
2023-10-28 22:28:39 +0200

    zad6

---
+++ zad6/zad6/Program.cs
@@ -0,0 +1,19 @@
+﻿Zadanie6();
+
+static void Zadanie6()
+{
+    Console.WriteLine("Nieskonczona petla podawania liczb calkowitych");
+    Console.WriteLine("Przerwiesz ja gdy podasz liczbe mniejsza niz 0");
+    Console.WriteLine("Press enter to continue");
+    Console.ReadLine();
+    System.Console.Clear();
+    while (true)
+    {
+        Console.WriteLine("Podaj liczbe calkowita: ");
+        if (!Int32.TryParse(Console.ReadLine(), out int input)) Console.WriteLine("Bledne dane");
+        else
+        {
+             if (input < 0) break;
+        }
+    }
+}
